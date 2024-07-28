93cd030e468b5ebffe191a3aa7e468483144d783
Marco Antonio Leite <marcofleitepro@gmail.com>
2024-07-28 01:51:04 -0300

    Create PORT System.getenv

--- src/main/java/me/dio/SantanderJavaBackend2024Application.java
+++ src/main/java/me/dio/SantanderJavaBackend2024Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SantanderJavaBackend2024Application {
 
+	public String PORT = System.getenv("PORT");
+
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderJavaBackend2024Application.class, args);
 	}
